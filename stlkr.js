7e865a934ff2587f922f061b8d6edfa51c8578f6
Gallienus <anorama@protonmail.com>
2018-10-14 20:15:52 +0200

    u

--- stlkr.js
+++ stlkr.js
@@ -8,11 +8,12 @@ console.log('  --------------------------------\n')
 if (process.argv.length <= 2) {
     // Usage: node stlkr <mode>
     console.log('Usage: node stlkr <mode>')
-    console.log(' Where mode is one of the following:')
+    console.log(' Where <mode> is one of the following:')
     console.log(' - full')
+    console.log(' - removeduplicates')
+    console.log(' - ensuremd5') // make sure all pictures in storage has md5 sig
     console.log(' - webserveronly')
     console.log(' - downloadonly')
-    console.log(' - removeduplicates') // remove duplicate media in storage folder
 } else {
     let mode = process.argv[2].toLowerCase()
 
@@ -22,8 +23,34 @@ if (process.argv.length <= 2) {
         require('webserver')
     }
 
-
     else if (mode === 'removeduplicates') {
-        console.log('Starting StlkR in <removeduplicates> mode.')
+        console.log('Starting StlkR in <RemoveDuplicates> mode.')
+
+        if (process.argv.length <= 3) {
+            // Usage: node stlkr removeduplicates <table>
+            console.log('Usage: node stlkr removeduplicates <table>')
+            console.log(' Where <table> is one of the following:')
+            console.log(' - user_real_name')
+            console.log(' - user_biography')
+            console.log(' - user_web_page')
+        } else {
+            let table = process.argv[3].toLocaleLowerCase()
+    
+            if (table === 'user_real_name'
+                    || table === 'user_biography'
+                    || table === 'user_web_page') {
+                require('./lib/robot/tools').mergeRealNameDuplicates(table)
+            }
+
+            else if (table === '') {
+
+            }
+        }
+    }
+
+    else if (mode === 'ensuremd5') {
+        console.log('Starting StlkR in <EnsureMD5> mode.')
+
+        require('./lib/robot/tools').ensureMd5()
     }
 }
